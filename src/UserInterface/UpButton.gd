e89aa291f0122917c9dc88f7968b7d1424ac4571
Luke <luke.beamish@peak.ai>
2022-03-15 16:37:49 +0000

    Switched up button to touchscreen button

--- src/UserInterface/UpButton.gd
+++ src/UserInterface/UpButton.gd
@@ -1,15 +1,15 @@
-extends TextureButton
-
-
-func _on_Button_button_up() -> void:
-	var new_action: InputEventAction = InputEventAction.new()
-	new_action.action = "jump"
-	new_action.pressed = false
-	Input.parse_input_event(new_action)
-
-
-func _on_Button_button_down() -> void:
-	var new_action: InputEventAction = InputEventAction.new()
-	new_action.action = "jump"
-	new_action.pressed = true
-	Input.parse_input_event(new_action)
+extends TouchScreenButton
+#
+#
+#func _on_Button_button_up() -> void:
+#	var new_action: InputEventAction = InputEventAction.new()
+#	new_action.action = "jump"
+#	new_action.pressed = false
+#	Input.parse_input_event(new_action)
+#
+#
+#func _on_Button_button_down() -> void:
+#	var new_action: InputEventAction = InputEventAction.new()
+#	new_action.action = "jump"
+#	new_action.pressed = true
+#	Input.parse_input_event(new_action)
